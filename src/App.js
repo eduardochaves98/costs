6897ccadf0e555e5c69903c7086c7b2604587fdb
Eduardo Chaves <80068125+eduardochaves98@users.noreply.github.com>
2023-11-22 20:07:24 +0000

    Final Primeira Aula

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Outlet, Link} from "react-router-dom";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
     <div className="App">
-      Costs
+      <ul className="App-header">
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Outlet />
+      </Container>
+      <p>Footer</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/contact",
+        element: <Contact/>
+      },
+      {
+        path: "/company",
+        element: <Company/>
+      },
+      {
+        path:"/newproject",
+        element: <NewProject/>
+      }
+    ]
+  }
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
